5cae732afb656db9070493f2aea0f4853407b814
khubaib Ahmed <144923555+852911@users.noreply.github.com>
2025-03-20 16:35:18 +0500

    Update authMiddleware.js

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -9,14 +9,11 @@ module.exports = async (req, res, next) => {
             return res.status(401).json({ message: "Access denied. No valid token provided." });
         }
 
-        // Extract actual token from "Bearer <token>"
         const token = authHeader.split(" ")[1];
 
-        // Verify JWT token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log("Decoded Token:", decoded);
 
-        // Fetch user from database
         const [rows] = await db.query(
             "SELECT user_id, role FROM users WHERE user_id = ?",
             [decoded.user_id]
@@ -26,13 +23,12 @@ module.exports = async (req, res, next) => {
             return res.status(403).json({ message: "Forbidden: Invalid user" });
         }
 
-        // Attach user details to request
         req.user = {
             user_id: rows[0].user_id,
             role: rows[0].role,
         };
 
-        next(); // Proceed to next middleware or route handler
+        next(); 
 
     } catch (error) {
         console.error("JWT Error:", error.message);
