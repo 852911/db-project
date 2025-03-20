f108f6928929027dc2d2c0b74d348f9d3dea7dcc
khubaib Ahmed <144923555+852911@users.noreply.github.com>
2025-03-20 16:34:31 +0500

    Update adminMiddleware.js

--- backend/src/middlewares/adminMiddleware.js
+++ backend/src/middlewares/adminMiddleware.js
@@ -7,7 +7,7 @@ const adminMiddleware = (req, res, next) => {
         return res.status(403).json({ message: "Access denied. Admins only." });
     }
 
-    next(); // Proceed to the next middleware or route handler
+    next();
 };
 
 module.exports = adminMiddleware;
